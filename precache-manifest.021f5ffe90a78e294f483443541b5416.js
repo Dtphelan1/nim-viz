e9ecb34f7579fa2d22fcb7554c904419712c0808
Dylan Phelan <dtphelan1@gmail.com>
2019-09-01 23:41:19 -0400

    Updates

---
+++ precache-manifest.021f5ffe90a78e294f483443541b5416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c550bafcd0472efa2e7ca8627bb7b0",
+    "url": "/nim-viz/index.html"
+  },
+  {
+    "revision": "961987338cb17ca38b4c",
+    "url": "/nim-viz/static/css/2.a8b2508a.chunk.css"
+  },
+  {
+    "revision": "bf32a1b9628bbea6d56a",
+    "url": "/nim-viz/static/css/main.fc0dc297.chunk.css"
+  },
+  {
+    "revision": "961987338cb17ca38b4c",
+    "url": "/nim-viz/static/js/2.0b0265ab.chunk.js"
+  },
+  {
+    "revision": "bf32a1b9628bbea6d56a",
+    "url": "/nim-viz/static/js/main.1081a290.chunk.js"
+  },
+  {
+    "revision": "a19f2964b1f67dfa0e4b",
+    "url": "/nim-viz/static/js/runtime~main.a3bd41ce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/nim-viz/precache-manifest.6db7c56839cde74bb7345be4d0f218c1.js"
+  "/nim-viz/precache-manifest.021f5ffe90a78e294f483443541b5416.js"
 );
 
 self.addEventListener('message', (event) => {
